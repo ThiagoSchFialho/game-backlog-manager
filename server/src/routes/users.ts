69a43a876f0ad26de9748d709f968476d4f767da
Thiago Scheffer Fialho <thiago.sch.fialho@gmail.com>
2026-08-22 18:38:16 -0300

    update and delete user routes

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -6,7 +6,7 @@ const usersModel = new UsersModel();
 const router = express.Router();
 
 router.post('/', async function (req: Request, res: Response) {
-  const { steam_id, steam_api_key } = req.body;
+  const { steam_id, steam_api_key } = req.body || {};
   
   if (!steam_id || !steam_api_key) {
     return res.status(400).json({ error: "Dados necessarios não informados para a criação do usuário." });
@@ -42,6 +42,55 @@ router.get('/', async function (req: Request, res: Response) {
     }
 
     return res.status(200).json(user);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Erro interno do servidor." });
+  }
+});
+
+router.put('/', async function (req: Request, res: Response) {
+  const { steam_api_key } = req.body || {};
+  
+  if (!steam_api_key || steam_api_key === undefined) {
+    return res.status(400).json({ error: "Chave da api não informada." });
+  }
+
+  try {
+    const user = await usersModel.getUser();
+
+    if (!user) {
+      return res.status(404).json({ error: "Usuário não encontrado." });
+    }
+
+    const updatedUser = await usersModel.updateApiKey(user.steam_id, steam_api_key);
+
+    if (!updatedUser) {
+      return res.status(500).json({ error: "Erro ao atualizar usuário no banco de dados." });
+    }
+
+    return res.status(200).json(updatedUser);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Erro interno do servidor." });
+  }
+});
+
+router.delete('/', async function (req: Request, res: Response) {
+  const { steam_id } = req.body || {};
+
+  if (!steam_id) {
+    return res.status(400).json({ error: "id steam não informada." });
+  }
+
+  try {
+    const user = await usersModel.deleteUser(steam_id);
+
+    if (!user) {
+      return res.status(500).json({ error: "Erro ao excluir usuário no banco de dados." });
+    }
+
+    return res.status(200).json(user);
+
   } catch (error) {
     console.error(error);
     return res.status(500).json({ error: "Erro interno do servidor." });
