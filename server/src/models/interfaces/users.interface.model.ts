0de54c2c6a0ff1b3066732958c73e5e12c1f3403
Thiago Scheffer Fialho <thiago.sch.fialho@gmail.com>
2026-08-21 22:11:11 -0300

    users model

--- server/src/models/interfaces/users.interface.model.ts
+++ server/src/models/interfaces/users.interface.model.ts
@@ -2,7 +2,7 @@ import { Users } from "../../entities/users";
 
 export interface IUserModel {
     createUser(steam_id: number, steam_api_key: string): Promise<Users>;
-    getUser(): Promise<Users | null>;
-    updateUser(steam_id: number, steam_api_key: string): Promise<Users | null>;
-    deleteUser(steam_id: number): Promise<boolean>;
+    getUser(): Promise<Users | undefined>;
+    updateApiKey(steam_id: number, steam_api_key: string): Promise<Users | undefined>;
+    deleteUser(steam_id: number): Promise<Users | undefined>;
 }
