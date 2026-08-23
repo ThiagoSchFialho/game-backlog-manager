48f8dc87194c9cf2bd20b446bb30d10b11b59ebd
Thiago Scheffer Fialho <thiago.sch.fialho@gmail.com>
2026-08-23 11:14:54 -0300

    game genres routes

--- server/src/config/db.init.sql
+++ server/src/config/db.init.sql
@@ -23,15 +23,16 @@ CREATE TABLE genres (
 );
 
 CREATE TABLE game_genres (
+    id BIGINT PRIMARY KEY,
     game_id BIGINT NOT NULL,
     genre_id BIGINT NOT NULL,
-    PRIMARY KEY (game_id, genre_id),
     FOREIGN KEY (game_id)
         REFERENCES games(id)
         ON DELETE CASCADE,
     FOREIGN KEY (genre_id)
         REFERENCES genres(id)
-        ON DELETE CASCADE
+        ON DELETE CASCADE,
+    UNIQUE (game_id, genre_id)
 );
 
 CREATE INDEX ON game_genres(genre_id);
