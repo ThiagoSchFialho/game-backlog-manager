0d28fa6754d8c8a888a5b6c070deaa89335e0891
Thiago Scheffer Fialho <thiago.sch.fialho@gmail.com>
2026-08-24 22:19:17 -0300

    header component

---
+++ client/src/components/Header/Header.tsx
@@ -0,0 +1,33 @@
+import './styles.css';
+import logo from '../../assets/GAME BACKLOG logo.svg';
+import searchIcon from '../../assets/search.svg';
+import steamIcon from '../../assets/steam.svg';
+
+interface HeaderProps {
+    steamApiConnected: boolean;
+}
+
+const Header = ({steamApiConnected}: HeaderProps) => {
+    return (
+        <>
+            <div className="header">
+                <img src={logo} alt="game backlog manager logo" />
+                <div className="search-bar-container">
+                    <img src={searchIcon} alt="icone de pesquisa" />
+                    <input className="search-input" type="text" name="search" id="search" placeholder="Pesquisar jogos, desenvolvedoras, tags..." />
+                </div>
+                <div className="steam-api-indicator-container">
+                    <img src={steamIcon} alt="steam logo" />
+                    <div className="steam-api-indicator-text-container">
+                        <p>Steam API</p>
+                        <p className={`steam-api-indicator ${steamApiConnected ? 'connected' : 'disconnected'}`}>
+                            {steamApiConnected ? "conectado" : "não conectado"}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Header;
--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,9 +1,13 @@
+import { useState } from 'react';
 import './styles.css';
+import Header from '../../components/Header/Header';
 
 const Home = () => {
+    const [steamApiConnected, setSteamApiConnected] = useState(false);
+
     return (
         <>
-            <p>home</p>
+            <Header steamApiConnected={steamApiConnected} />
         </>
     )
 }
