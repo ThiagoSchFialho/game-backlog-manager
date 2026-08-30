9d0016b90d2c3f5ca570887a89fdc59979de3596
Thiago Scheffer Fialho <thiago.sch.fialho@gmail.com>
2026-08-29 21:43:18 -0300

    steam sync form

--- client/src/pages/Settings/Settings.tsx
+++ client/src/pages/Settings/Settings.tsx
@@ -9,6 +9,9 @@ const Settings: React.FC = () => {
     const [steamApiConnected, setSteamApiConnected] = useState(false);
     const [currentPage, setCurrentPage] = useState('settings');
 
+    const [steamId, setSteamId] = useState('');
+    const [steamApiKey, setSteamApiKey] = useState('');
+
     return (
         <>
             <Header steamApiConnected={steamApiConnected} />
@@ -21,18 +24,34 @@ const Settings: React.FC = () => {
                     </div>
                         <div className="steam-section">
                             <h1 className="steam-section-title">Steam</h1>
-                            <form className="steam-section-form">
+                            <form className="steam-section-form" onSubmit={(e) => e.preventDefault()}>
                                 <div className="input-label-container">
                                     <label htmlFor="steam-id">ID da sua conta Steam</label>
                                     <div className="input-container">
-                                        <input type="text" name="steam-id" id="steam-id" maxLength={17} minLength={17} />
+                                        <input 
+                                            onChange={(e) => setSteamId(e.target.value)}
+                                            value={steamId} 
+                                            type="text" 
+                                            name="steam-id" 
+                                            id="steam-id" 
+                                            maxLength={17} 
+                                            minLength={17}
+                                        />
                                         <img src={info} alt="informação" />
                                     </div>
                                 </div>
                                 <div className="input-label-container">
                                     <label htmlFor="steam-key">Sua chave da API</label>
                                     <div className="input-container"> 
-                                        <input type="text" name="steam-key" id="steam-key" maxLength={33} minLength={33} />
+                                        <input 
+                                            onChange={(e) => setSteamApiKey(e.target.value)}
+                                            value={steamApiKey} 
+                                            type="text" 
+                                            name="steam-key" 
+                                            id="steam-key" 
+                                            maxLength={33} 
+                                            minLength={33}
+                                        />
                                         <img src={info} alt="informação" />
                                     </div>
                                 </div>
