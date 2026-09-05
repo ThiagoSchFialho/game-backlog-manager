fa509550241b7f07714a1844ba7d8391b4b518d2
Thiago Scheffer Fialho <thiago.sch.fialho@gmail.com>
2026-09-04 21:53:23 -0300

    collapsible lists on backlog page

--- client/src/pages/Backlog/Backlog.tsx
+++ client/src/pages/Backlog/Backlog.tsx
@@ -8,12 +8,15 @@ import type { Game } from '../../types/gamesType';
 import { useDb } from '../../hooks/useDb';
 import { orderBy } from '../../utils/orderBy';
 import SyncSteamBtn from '../../components/syncSteamBtn/SyncSteamBtn';
+import arrow from '../../assets/icons/menu-arrow.svg';
 
 const Backlog: React.FC = () => {
     const { fetchGames } = useDb();
     const [steamApiConnected, setSteamApiConnected] = useState(false);
     const [currentPage] = useState('backlog');
     const [gamesList, setGamesList] = useState<Game[]>([]);
+    const [isPlayedSectionOpen, setIsPlayedSectionOpen] = useState(false);
+    const [isNotPlayedSectionOpen, setIsNotPlayedSectionOpen] = useState(false);
     
     useEffect(() => {
         const getGames = async () => {
@@ -43,56 +46,96 @@ const Backlog: React.FC = () => {
                         <SyncSteamBtn />
                     </div>
 
-                    <div className="backlog-games-container backlog-playing-games">
-                        <h1 className="backlog-games-container-title">Jogando</h1>
-                        <div className="game-container">
-                            {playingGames.slice(0, 5).map(game => (
-                                <GameCard
-                                    key={game.id}
-                                    id={game.id}
-                                    steamId={game.steam_id}
-                                    img={getGameCover(game.title)}
-                                    name={game.title}
-                                    status={game.status}
-                                    playtime={game.playtime}
-                                />
-                            ))}
+                    <section id="playing-section">
+                        <div className="backlog-games-container backlog-playing-games">
+                            <h1 className="backlog-games-container-title">Jogando</h1>
+                            <div className="game-container">
+                                {playingGames.slice(0, 5).map(game => (
+                                    <GameCard
+                                        key={game.id}
+                                        id={game.id}
+                                        steamId={game.steam_id}
+                                        img={getGameCover(game.title)}
+                                        name={game.title}
+                                        status={game.status}
+                                        playtime={game.playtime}
+                                    />
+                                ))}
+                            </div>
                         </div>
-                    </div>
+                    </section>
 
-                    <div className="backlog-games-container backlog-played-games">
-                        <h1 className="backlog-games-container-title">Jogado</h1>
-                        <div className="game-container">
-                            {playedGames.map(game => (
-                                <GameCard
-                                    key={game.id}
-                                    id={game.id}
-                                    steamId={game.steam_id}
-                                    img={getGameCover(game.title)}
-                                    name={game.title}
-                                    status={game.status}
-                                    playtime={game.playtime}
-                                />
-                            ))}
-                        </div>
-                    </div>
-
-                    <div className="backlog-games-container backlog-not-played-games">
-                        <h1 className="backlog-games-container-title">Não jogado</h1>
-                        <div className="game-container">
-                            {notPlayedGames.map(game => (
-                                <GameCard
-                                    key={game.id}
-                                    id={game.id}
-                                    steamId={game.steam_id}
-                                    img={getGameCover(game.title)}
-                                    name={game.title}
-                                    status={game.status}
-                                    playtime={game.playtime}
-                                />
-                            ))}
-                        </div>
-                    </div>
+                    <section id="played-section">
+                            <div className="backlog-games-container backlog-played-games">
+                                <div className="backlog-games-container-header">
+                                    <h1 className="backlog-games-container-title">Jogado</h1>
+                                    <img
+                                        onClick={() => setIsPlayedSectionOpen(!isPlayedSectionOpen)}
+                                        className={isPlayedSectionOpen ? "expand-arrow-active" : "expand-arrow"}
+                                        src={arrow}
+                                    />
+                                </div>
+                                <div className="game-container">
+                                    {isPlayedSectionOpen ? playedGames.map(game => (
+                                        <GameCard
+                                            key={game.id}
+                                            id={game.id}
+                                            steamId={game.steam_id}
+                                            img={getGameCover(game.title)}
+                                            name={game.title}
+                                            status={game.status}
+                                            playtime={game.playtime}
+                                        />
+                                    )) : playedGames.slice(0, 5).map(game => (
+                                        <GameCard
+                                            key={game.id}
+                                            id={game.id}
+                                            steamId={game.steam_id}
+                                            img={getGameCover(game.title)}
+                                            name={game.title}
+                                            status={game.status}
+                                            playtime={game.playtime}
+                                        />
+                                    ))}
+                                </div>
+                            </div>
+                    </section>
+                    
+                    <section id="not-played-section">
+                            <div className="backlog-games-container backlog-not-played-games">
+                                <div className="backlog-games-container-header">
+                                    <h1 className="backlog-games-container-title">Não jogado</h1>
+                                    <img
+                                        onClick={() => setIsNotPlayedSectionOpen(!isNotPlayedSectionOpen)}
+                                        className={isNotPlayedSectionOpen ? "expand-arrow-active" : "expand-arrow"}
+                                        src={arrow}
+                                    />
+                                </div>
+                                <div className="game-container">
+                                    {isNotPlayedSectionOpen ? notPlayedGames.map(game => (
+                                        <GameCard
+                                            key={game.id}
+                                            id={game.id}
+                                            steamId={game.steam_id}
+                                            img={getGameCover(game.title)}
+                                            name={game.title}
+                                            status={game.status}
+                                            playtime={game.playtime}
+                                        />
+                                    )) : notPlayedGames.slice(0, 5).map(game => (
+                                        <GameCard
+                                            key={game.id}
+                                            id={game.id}
+                                            steamId={game.steam_id}
+                                            img={getGameCover(game.title)}
+                                            name={game.title}
+                                            status={game.status}
+                                            playtime={game.playtime}
+                                        />
+                                    ))}
+                                </div>
+                            </div>
+                    </section>
                 </div>
             </div>
         </>
