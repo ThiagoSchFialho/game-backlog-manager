c9ff7200d4a1df4a86411098b64622e496993fd4
Thiago Scheffer Fialho <thiago.sch.fialho@gmail.com>
2026-09-04 23:35:47 -0300

    fix: electron build for typescript

--- client/src/routes/Routes.tsx
+++ client/src/routes/Routes.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home/Home';
 import AllGames from '../pages/AllGames/AllGames';
 import Favorites from '../pages/Favorites/Favorites';
@@ -11,7 +11,7 @@ import Settings from '../pages/Settings/Settings';
 
 const AppRoutes = () => {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/allGames' element={<AllGames />} />
@@ -22,7 +22,7 @@ const AppRoutes = () => {
                 <Route path='/collection/:id' element={<Collection />} />
                 <Route path='/settings' element={<Settings />} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     )
 }
 
